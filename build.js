3764da93f3478ffd0de30392d6f6ca02dc3fd371
Dylan Depass <dylandepass@gmail.com>
2022-10-20 21:46:24 -0400

    fix: correct units

---
+++ build.js
@@ -0,0 +1,24 @@
+const StyleDictionary = require('style-dictionary');
+const baseConfig = require('./style-dictionary.config.json')
+
+StyleDictionary.registerTransform({
+    name: 'sizes/px',
+    type: 'value',
+    matcher: function(prop) {
+        // You can be more specific here if you only want 'em' units for font sizes    
+        return ["fontSizes", "fontSize", "lineHeights", "lineHeight", "sizing", "spacing"].includes(prop.type);
+    },
+    transformer: function(prop) {
+        // You can also modify the value here if you want to convert pixels to ems
+        return parseFloat(prop.original.value) + 'px';
+    }
+    });
+
+console.log('Build started...');
+
+const sd = StyleDictionary.extend(baseConfig)
+
+sd.buildAllPlatforms()
+console.log('\n==============================================');
+
+console.log('\nBuild completed!!');
